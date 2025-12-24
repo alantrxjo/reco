b54949baa5012f104356a958b82605ce32dcdc68
alantrxjo <abalderastrejo@gmail.com>
2025-12-24 10:55:46 -0600

    feat: change the structure of the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './modules/home/home';
+import { Test } from './modules/test/test';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: Home },
+    { path: 'test', component: Test },
+
+    /*Detecta cualquier url no encontrada*/
+    { path: '**', redirectTo: 'home' },
+];
